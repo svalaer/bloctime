76821faac87c804b69c01518607b9bfc098388d8
svalaer <sullivanvalaer@gmail.com>
2015-04-01 17:55:30 -0700

    Installed humanize-duration and moment js using bower.

---
+++ app/scripts/app.js
@@ -0,0 +1,7 @@
+//require('./firebase');
+//require('./greyscale');
+//require('./main');
+
+angular.module('Bloctime', []).controller('Landing.controller', ['$scope', function($scope) {
+    console.log("Landing.controller");
+}]);
