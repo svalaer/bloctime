49d3760f2149d71bedcba3aae40b90322aa139e1
svalaer <sullivanvalaer@gmail.com>
2015-04-08 09:37:12 -0700

    Moved the app to a single page app.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,27 @@
 //require('./greyscale');
 //require('./main');
 
-var Bloctime = angular.module('Bloctime', []);
-    Bloctime.controller('Landing.controller', ['$scope', function($scope) {
-    console.log("Landing.controller");
+var Bloctime = angular.module('Bloctime', ['ui.router']);
+
+Bloctime.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $stateProvider.state('landing', {
+        url: '/',
+        controller: 'Landing.controller',
+        templateUrl: '/dist/index.html'
+    });
+    $stateProvider.state('timer', {
+        url: '/timer',
+        controller: 'Timer.controller',
+        templateUrl: '/dist/timer.html'
+    });
+}]);
+
+Bloctime.controller('Landing.controller', ['$scope', function($scope) {
+    console.log("test");
+}]);
+
+Bloctime.controller('Timer.controller', ['$scope', function($scope) {
+    console.log("test");
 }]);
