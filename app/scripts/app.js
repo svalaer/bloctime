8304402cf47366a5303bf8c38bc873c43f2d661c
svalaer <sullivanvalaer@gmail.com>
2015-05-03 17:22:00 -0700

    Added google maps api into landing page. Positioning of elements seems off. Bootstrap problems?

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,64 +32,57 @@ Bloctime.controller('Timer.controller', ['$scope', function($scope) {
 
 Bloctime.constant("FIREBASE_URL", "'https://blinding-inferno-1918.firebaseio.com/'" );
 
-Bloctime.controller('DemoCtrl', ['$scope', '$firebaseObject', "FIREBASE_URL", function ($scope, $firebase, FIREBASE_URL) {
+Bloctime.controller('DemoCtrl', ['$scope', '$firebaseArray', "FIREBASE_URL", function ($scope, $firebaseArray, FIREBASE_URL) {
 
     // Get Stored TODOs
-    var todosRef = new Firebase('https://blinding-inferno-1918.firebaseio.com/');
-    $scope.todos = $firebase(todosRef);
+    var todosRef = new Firebase('https://blinding-inferno-1918.firebaseio.com/todos');
+    $scope.todos = $firebaseArray(todosRef);
 
 
     // Update the "completed" status
-    $scope.changeStatus   = function (item) {
-
-        // Get the Firebase reference of the item
-        var itemRef = new  Firebase('https://blinding-inferno-1918.firebaseio.com/' + item.id);
-
-        // Firebase : Update the item
-        $firebase(itemRef).$set({
-            id: item.id,
-            name : item.name,
-            completed: !item.completed
-        });
-
+    $scope.updateItem = function($id) {
+        var item = $scope.todos.$getRecord($id);
+        item.status = "Completed";
+        $scope.todos.$save(item);
     };
+
+    //$scope.changeStatus   = function(item) {
+    //
+    //    // Get the Firebase reference of the item
+    //    var itemRef = new  Firebase('https://blinding-inferno-1918.firebaseio.com/' + item.id);
+    //
+    //    // Firebase : Update the item
+    //    $firebaseArray(itemRef).set({
+    //        id: item.id,
+    //        name : item.name,
+    //        completed: !item.completed
+    //    });
+    //
+    //};
 
 
 
     // Remove a Todo
-    $scope.removeItem   = function (index, item, event) {
-
-        // Avoid wrong removing
-        if (item.id == undefined)return;
-
-        // Firebase: Remove item from the list
-        $scope.todos.$remove(item.id);
+    $scope.removeItem  = function($index) {
+        $scope.todos.$remove($index)
 
     };
 
 
 
     // Add new TODO
-    $scope.addItem  = function () {
+    $scope.addItem  = function(name) {
+        $scope.todos.$add({
+            name: name
+        })
 
-        // Create a unique ID
-        var timestamp = new Date().valueOf();
-
-        // Get the Firebase reference of the item
-        var itemRef = new Firebase('https://blinding-inferno-1918.firebaseio.com/' + timestamp);
-
-        $firebase(itemRef).$set({
-            id: timestamp,
-            name : $scope.todoName,
-            completed: false
-        });
-
-        $scope.todoName = "";
 
     };
 
 
 }]);
+
+var map = new google.maps.Map();
 
 
 
